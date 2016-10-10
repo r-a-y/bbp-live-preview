4add7e4d8219e797d8ff727cd99d08425e4864c5
r-a-y <r-a-y@gmx.com>
2016-10-10 14:37:26 -0700

    Tabs instead of spaces.

--- assets/scripts.js
+++ assets/scripts.js
@@ -3,96 +3,96 @@ var bbp_preview_timer      = null;
 var bbp_preview_ajaxurl    = bbpLivePreviewInfo.ajaxUrl;
 
 function bbp_preview_post( text, type, tinymce ) {
-    tinymce = typeof tinymce !== 'undefined';
-    clearTimeout(bbp_preview_timer);
+	tinymce = typeof tinymce !== 'undefined';
+	clearTimeout(bbp_preview_timer);
 
-    // @todo Allow timeout variable to be configured.
-    bbp_preview_timer = setTimeout(function(){
-        var post = jQuery.post(
-            bbp_preview_ajaxurl,
-            {
-                action: 'bbp_live_preview',
-                'text': text,
-                'type': type,
-                'tinymce' : tinymce
-            }
-        );
+	// @todo Allow timeout variable to be configured.
+	bbp_preview_timer = setTimeout(function(){
+		var post = jQuery.post(
+			bbp_preview_ajaxurl,
+			{
+				action: 'bbp_live_preview',
+				'text': text,
+				'type': type,
+				'tinymce' : tinymce
+			}
+		);
 
-        post.success( function (data) {
-            jQuery("#bbp-post-preview").html(data);
-            if ( ! bbp_preview_is_visible ) {
-                jQuery( '#bbp-post-preview-wrapper' ).slideDown();
-                bbp_preview_is_visible = true;
-            }
-            jQuery("#bbp-post-preview").removeClass('loading');
-            jQuery("#bbp-post-preview").trigger('loaded.bbp_live_preview');
-        });
-    }, 1500);
+		post.success( function (data) {
+			jQuery("#bbp-post-preview").html(data);
+			if ( ! bbp_preview_is_visible ) {
+				jQuery( '#bbp-post-preview-wrapper' ).slideDown();
+				bbp_preview_is_visible = true;
+			}
+			jQuery("#bbp-post-preview").removeClass('loading');
+			jQuery("#bbp-post-preview").trigger('loaded.bbp_live_preview');
+		});
+	}, 1500);
 
 }
 
 // tinymce capture
 if ( bbpLivePreviewInfo.tinymceFourthPlusVersion ) {
-    jQuery(document).load(function(){
-        var formType = bbpLivePreviewInfo.formType;
-        var contentSelector = 'bbp_' + formType + '_content';
-        tinymce.get(contentSelector).on('keyup', function (e) {
-            bbp_preview_post(this.getContent(), formType, true);
-        });
-    });
+	jQuery(document).load(function(){
+		var formType = bbpLivePreviewInfo.formType;
+		var contentSelector = 'bbp_' + formType + '_content';
+		tinymce.get(contentSelector).on('keyup', function (e) {
+			bbp_preview_post(this.getContent(), formType, true);
+		});
+	});
 } else {
-    function bbp_preview_tinymce_capture(e) {
-        if (e.type == 'keyup') {
-            var id = e.view.frameElement.id.split('_');
+	function bbp_preview_tinymce_capture(e) {
+		if (e.type == 'keyup') {
+			var id = e.view.frameElement.id.split('_');
 
-            bbp_preview_post(e.target.innerHTML, id[1]);
-        }
-    }
+			bbp_preview_post(e.target.innerHTML, id[1]);
+		}
+	}
 }
 
 // regular textarea capture
 jQuery(document).ready( function($) {
-    // override default 'hide' jQuery method to add a trigger
-    // used for the quicktags 'link' button
-    var _oldhide = $.fn.hide;
-    $.fn.hide = function(speed, callback) {
-        $(this).trigger('hide');
-        return _oldhide.apply(this,arguments);
-    };
+	// override default 'hide' jQuery method to add a trigger
+	// used for the quicktags 'link' button
+	var _oldhide = $.fn.hide;
+	$.fn.hide = function(speed, callback) {
+		$(this).trigger('hide');
+		return _oldhide.apply(this,arguments);
+	};
 
-    // keyboard input
-    $(".wp-editor-container").on("keyup", "#bbp_topic_content, #bbp_reply_content", function() {
-        var textarea = $(this);
-        jQuery("#bbp-post-preview").addClass('loading');
-        bbp_preview_post( textarea.val(), textarea.attr('id').split('_')[1] );
+	// keyboard input
+	$(".wp-editor-container").on("keyup", "#bbp_topic_content, #bbp_reply_content", function() {
+		var textarea = $(this);
+		jQuery("#bbp-post-preview").addClass('loading');
+		bbp_preview_post( textarea.val(), textarea.attr('id').split('_')[1] );
 
-    });
+	});
 
-    // quicktags toolbar support
-    $(".wp-editor-container").on("click", ".quicktags-toolbar", function(e) {
-        // do not do anything for 'link' button that's handled below
-        if ( 'link' === e.target.value ) {
-            return;
-        }
+	// quicktags toolbar support
+	$(".wp-editor-container").on("click", ".quicktags-toolbar", function(e) {
+		// do not do anything for 'link' button that's handled below
+		if ( 'link' === e.target.value ) {
+			return;
+		}
 
-        var textarea = $(this).parent().find('textarea');
-        jQuery("#bbp-post-preview").addClass('loading');
-        bbp_preview_post( textarea.val(), textarea.attr('id').split('_')[1] );
-    });
+		var textarea = $(this).parent().find('textarea');
+		jQuery("#bbp-post-preview").addClass('loading');
+		bbp_preview_post( textarea.val(), textarea.attr('id').split('_')[1] );
+	});
 
-    // quicktags link button
-    $(document).on('hide','#wp-link-wrap', function() {
-        var textarea, type;
-        if ( $('#bbp_topic_title').length ) {
-            type = 'topic';
+	// quicktags link button
+	$(document).on('hide','#wp-link-wrap', function() {
+		var textarea, type;
+		if ( $('#bbp_topic_title').length ) {
+			type = 'topic';
 
-        } else {
-            type = 'reply';
-        }
+		} else {
+			type = 'reply';
+		}
 
-        textarea = $( "#bbp_" + type + "_content");
-        jQuery("#bbp-post-preview").addClass('loading');
-        bbp_preview_post( textarea.val(), type );
-    });
+		textarea = $( "#bbp_" + type + "_content");
+		jQuery("#bbp-post-preview").addClass('loading');
+		bbp_preview_post( textarea.val(), type );
+	});
 
 });
